32186d668a9b336f21e24cf03d1abe935649538a
Janith Chathuranga Withanagamage <it21250156@my.sliit.lk>
2025-03-19 18:01:58 +0530

    ui fixings

--- frontend/src/layouts/HomeLayout.js
+++ frontend/src/layouts/HomeLayout.js
@@ -8,7 +8,7 @@ const HomeLayout = () => (
             <Sidebar />
         </div>
         {/* Scrollable Main Content */}
-        <main className="flex-1 ml-64 overflow-y-auto bg-gradient-to-r to-[#dfe2e6] from-[#e9f3ff]">
+        <main className="flex-1 ml-64 overflow-y-auto bg-gradient-to-r to-[#dfe2e6] from-[#ffffff]">
             <Outlet />
         </main>
     </div>
