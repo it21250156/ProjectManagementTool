0c5f8c641d877a9b040ee9160e034d610b25026b
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-05-25 19:24:07 +0530

    Performance Evaluation Added

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -84,15 +84,17 @@ const Sidebar = () => {
                             </button>
                         </Link>
 
-                        <button type="button" class="text-white bg-[#4a90e2] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-lg py-4 w-full mb-1 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                            My Teams
-                        </button>
-
                         <Link to="/my-profile">
                             <button type="button" class="text-white bg-[#4a90e2] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-lg py-4 w-full mb-1 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                                 My Profile
                             </button>
                         </Link>
+
+                        <Link to="/performance-evaluation">
+    <button type="button" className="text-white bg-[#4a90e2] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-lg py-4 w-full mb-1 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+        Performance
+    </button>
+</Link>
 
                         <Link to="/global-leaderboard">
                             <button type="button" class="text-white bg-[#4a90e2] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-lg py-4 w-full mb-1 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
